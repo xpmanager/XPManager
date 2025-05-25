df05de4ca7d3d10892f43abdd1e5aaad392c2435
Mohaned Mohammed Sherhan <shrhanmhnd15@gmail.com>
2025-05-25 11:07:27 +0300

    Added utilities doc comments

--- src/utilities/mod.rs
+++ src/utilities/mod.rs
@@ -7,12 +7,31 @@ use colored::Colorize;
 use rand::seq::{IndexedRandom, IteratorRandom};
 use std::path::PathBuf;
 
+/// The password sample types.
+/// uses to chooes sample to generate the password.
 #[derive(PartialEq)]
 pub enum PasswordSample {
     Ascii,
     NoSymbols,
     Hex
 }
+
+/// Generate sample based on the type.
+/// types from PasswordSample enum:
+/// - Ascii: A-Z, a-z, 0-9 and some symbols.
+/// - NoSymbols: A-Z, a-z and 0-9.
+/// - Hex: 0-9 and A-F.
+/// 
+/// ### Example:
+/// ```
+/// let sample = utilities::get_sample(
+///     utilities::PasswordSample::Hex
+/// );
+/// let hex: Vec<char> = ('0'..='9')
+///     .chain('A'..='F')
+///     .collect();
+/// assert_eq!(sample, hex);
+/// ```
 pub fn get_sample(sample: PasswordSample) -> Vec<char> {
     match sample {
         PasswordSample::Ascii => return ('a'..='z')
@@ -33,6 +52,14 @@ pub fn get_sample(sample: PasswordSample) -> Vec<char> {
     }
 }
 
+/// Generate random number as `String` between 32 to 73.
+/// 
+/// ## Example:
+/// ```
+/// let number_str = utilities::get_ran_string_number();
+/// let number = number_str.parse::<i32>().unwrap_or(0);
+/// assert_eq!(number >= 32 && number <= 72, true);
+/// ```
 pub fn get_ran_string_number() -> String {
     let mut rag = rand::rng();
     // Choose random length between 32 to 72.
@@ -42,6 +69,17 @@ pub fn get_ran_string_number() -> String {
         .to_string();
 }
 
+/// Read a string from standard input.
+/// 
+/// ### Example
+/// ```
+/// let input = utilities::input("Enter your name: ");
+/// if input == "Mohaned" {
+///     println!("Hello Mohaned");
+/// } else {
+///     println!("hi {}", input);
+/// }
+/// ```
 pub fn input(message: &str) -> String {
     use std::io::Write;
     print!("{}", message);
@@ -53,6 +91,19 @@ pub fn input(message: &str) -> String {
     return line.trim().to_owned();
 }
 
+/// Get user confirmation by entring 6-digit code.
+/// Exit the program if the confirmation fails.
+/// 
+/// ### Example:
+/// ```
+/// let mut logger = loglib::Logger::new("delete-file");
+/// logger.warning("file will be deleted!");
+/// 
+/// utilities::confirm(); // Waiting for the user...
+/// logger.start(); // Reset the counter, confirmation will take a while.
+/// 
+/// delete_file(PathBuf::new().join("./file.txt"));
+/// ```
 pub fn confirm() {
     let mut logger = loglib::Logger::new("confirm");
     logger.warning("This process requires confirmation!");
